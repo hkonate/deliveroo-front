48e82af640244830f27e17ca1acc3e5f4e3d612b
hkonate <konate.tronca@gmail.com>
2022-07-26 15:56:18 +0200

    delivroo-front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './assets/fonts.css'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const [data, setData] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  const fetchData = async () => {
+    const response = await axios.get("https://deliveroo-back-end-react.herokuapp.com/");
+
+    setData(response.data)
+    console.log(response.data)
+    setIsLoading(false)
+  }
+  useEffect(() => { fetchData() }, [])
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <div>
+      <header>
+        <div className='underline'><div className='container'><img src="/images/logo-teal.svg" alt="logo" /></div></div>
+
+        <div className="descrip container">
+          <div>
+            <h2>{data.restaurant.name}</h2>
+            <p>{data.restaurant.description}</p>
+          </div>
+          <img src="https://f.roocdn.com/images/menus/17697/header-image.jpg" alt="" />
+        </div>
       </header>
-    </div>
-  );
+      <div className='main'>
+        <div className='content'>
+          <div className='menu'>
+            {
+              data.categories.map((menu, index) => {
+                return (
+                  (menu.meals.length > 0) &&
+                  < div key={index} >
+                    <h3>{menu.name}</h3>
+                    <div className='menu-item'>
+                      {menu.meals.map((plate, i) => {
+                        return (<div key={i} className='meal'>
+                          <div className='meal-descrip'>
+                            <h4>{plate.title}</h4>
+                            <p>{plate.description}</p>
+                            <div className='meals-price'>
+                              <span>{plate.price} €</span>
+                              {plate.popular && <span>★ Populaire</span>}
+                            </div>
+                          </div>
+                          <div className='meals-pics'>
+                            {
+                              plate.picture && <img src={plate.picture} alt="plate" />
+                            }
+                          </div>
+                        </div>)
+                      })}
+                    </div>
+                  </div>)
+              })
+            }
+          </div>
+          <div className='cart'>
+            <button>Valider mon panier</button>
+            <p>Votre panier est vide</p>
+          </div>
+        </div>
+      </div>
+    </div >)
 }
 
 export default App;
